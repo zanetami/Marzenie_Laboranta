98e42241723544c1d87efa2f6b9a712ceeb23bbd
Żaneta Mielczarek <zmielcza@wasko.pl>
2019-10-10 13:02:44 +0200

    project starter

---
+++ lab_dream_backend/src/main/kotlin/lab_dream/LabDreamApplication.kt
@@ -0,0 +1,11 @@
+package lab_dream
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LabDreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<LabDreamApplication>(*args)
+}

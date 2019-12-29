a2521c64512bef67b8cf1232a41d1330053855d8
Zaneta <zaneta.mielczarek5@wp.pl>
2019-12-29 18:43:15 +0100

    dodano zmiane statusu i priorytetu zgloszenia

--- lab_dream/src/app/pages/notification-details/notification-details.page.ts
+++ lab_dream/src/app/pages/notification-details/notification-details.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Device } from 'src/app/models/device';
 import { ColumnMode } from '@swimlane/ngx-datatable';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notification-details',
@@ -15,26 +16,31 @@ export class NotificationDetailsPage implements OnInit {
   connectedDevices: Device[] = [];
   columns = [
     {
-      name: 'Identyfikator',
-      prop: 'id_d'
-    },
-    {
       name: 'Typ',
       prop: 'type'
     },
     {
-      name: 'Marka',
-      prop: 'brand'
+      name: 'Sala',
+      prop: 'lab'
     },
     {
-      name: 'Model/wersja',
-      prop: 'model'
-    }
+      name: 'Identyfikator',
+      prop: 'id_d'
+    },
+    // {
+    //   name: 'Marka',
+    //   prop: 'brand'
+    // },
+    // {
+    //   name: 'Model/wersja',
+    //   prop: 'model'
+    // }
   ];
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private alertController: AlertController,
+    private toastController: ToastController
   ) { 
     this.route.queryParams.subscribe(params => {
       if (params && params.issue) {
@@ -47,6 +53,42 @@ export class NotificationDetailsPage implements OnInit {
     this.getConnectedDevices();
   }
 
+  //#region popupy
+  async solveAlert() {
+    const alert = await this.alertController.create({
+      // header: 'Alert',
+      // subHeader: 'Subtitle',
+      message: 'Czy na pewno rozwiązać zgłoszenie?',
+      buttons: [
+        {
+          text: 'Rozwiązane',
+          role: 'solve',
+          handler: () => {
+            this.issue.state = 'Rozwiązane';
+            this.presentToast('Rozwiązano zgłoszenie', 1000);
+          }
+        },
+        {
+          text: 'Anuluj',
+          role: 'cancel',
+          handler: () => {
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentToast(message, duration) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: duration
+    });
+    toast.present();
+  }
+  //#endregion
+
   getConnectedDevices() {
     //this.connectedDevices = getfromAPI gdzie lab sie zgadza
     // id_d: number;
@@ -54,10 +96,54 @@ export class NotificationDetailsPage implements OnInit {
     // brand: string;
     // model: string;
     this.connectedDevices = [
-      {id_d: 'BHBi67', type: 'Sprzęt', brand: 'Samsung', model: 'Note5', lab: ''},
-      {id_d: 'QHJ78J', type: 'oprogramowanie', brand: 'mathematica', model: '8.1', lab: ''},
-      {id_d: 'MOX5D7', type: 'sieć', brand: 'internet', model: '', lab: ''},
+      {id_d: 'BHBi67', type: 'Sprzęt', brand: 'Samsung', model: 'Note5', lab: '435'},
+      {id_d: 'QHJ78J', type: 'oprogramowanie', brand: 'mathematica', model: '8.1', lab: '675'},
+      {id_d: 'MOX5D7', type: 'sieć', brand: 'internet', model: '', lab: '142'},
     ]
+  }
+
+  changePriority(direction) {
+    if ( this.issue.state !== 'Rozwiązane' ) {
+      if ( direction === 'up' ) {
+        switch (this.issue.priority) {
+          case 'Niski': {
+            this.issue.priority = 'Normalny';
+            break;
+          }
+          case 'Normalny': {
+            this.issue.priority = 'Wysoki';
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      } else {
+        switch (this.issue.priority) {
+          case 'Normalny': {
+            this.issue.priority = 'Niski';
+            break;
+          }
+          case 'Wysoki': {
+            this.issue.priority = 'Normalny';
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }
+    }
+    // przeslij do bazy zmiane
+  }
+
+  changeState() {
+    if (this.issue.state === 'Oczekuje') {
+      this.issue.state = 'W naprawie';
+    } else if ( this.issue.state === 'W naprawie' ) {
+      this.solveAlert();
+    }
+    // przeslij do bazy zmiane
   }
 
 }
--- lab_dream/src/app/pages/notifications/notifications.page.ts
+++ lab_dream/src/app/pages/notifications/notifications.page.ts
@@ -13,18 +13,18 @@ export class NotificationsPage implements OnInit {
   columnMode = ColumnMode;
   columns = [];
   rows = [
-    {id_i: '1', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '2', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '3', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '4', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '5', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '6', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '7', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '8', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '9', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '10', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '11', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
-    {id_i: '12', notif_d: '2019-11-12', lab: '410', priority: 'normal', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'done'},
+    {id_i: '1', notif_d: '2019-11-12', priority: 'Wysoki', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'W naprawie'},
+    {id_i: '2', notif_d: '2019-11-12', priority: 'Normalny', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Oczekuje'},
+    {id_i: '3', notif_d: '2019-11-12', priority: 'Niski', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Rozwiązane'},
+    {id_i: '4', notif_d: '2019-11-12', priority: 'Normalny', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Rozwiązane'},
+    {id_i: '5', notif_d: '2019-11-12', priority: 'Normalny', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Rozwiązane'},
+    {id_i: '6', notif_d: '2019-11-12', priority: 'Niski', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'W naprawie'},
+    {id_i: '7', notif_d: '2019-11-12', priority: 'Normalny', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Rozwiązane'},
+    {id_i: '8', notif_d: '2019-11-12', priority: 'Wysoki', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Rozwiązane'},
+    {id_i: '9', notif_d: '2019-11-12', priority: 'Wysoki', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Rozwiązane'},
+    {id_i: '10', notif_d: '2019-11-12', priority: 'Normalny', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'W naprawie'},
+    {id_i: '11', notif_d: '2019-11-12', priority: 'Niski', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Oczekuje'},
+    {id_i: '12', notif_d: '2019-11-12', priority: 'Niski', descr: 'adasdsadsadas as dsad as asd as', accept_d: '2019-11-12', solve_d: '2019-11-12', solver_id: '3', state: 'Oczekuje'},
   ];
   userRole = 'user';
 
@@ -49,10 +49,6 @@ export class NotificationsPage implements OnInit {
           prop: 'notif_d'
         },
         {
-          name: 'Sala',
-          prop: 'lab'
-        },
-        {
           name: 'Opis',
           prop: 'descr'
         },
@@ -72,10 +68,6 @@ export class NotificationsPage implements OnInit {
           prop: 'solve_d'
         },
         {
-          name: 'Sala',
-          prop: 'lab'
-        },
-        {
           name: 'Priorytet',
           prop: 'priority'
         },
@@ -91,16 +83,16 @@ export class NotificationsPage implements OnInit {
           prop: 'notif_d'
         },
         {
-          name: 'Sala',
-          prop: 'lab'
-        },
-        {
           name: 'Wykonawca',
           prop: 'solver_id'
         },
         {
           name: 'Status',
           prop: 'state'
+        },
+        {
+          name: 'Priorytet',
+          prop: 'priority'
         }
       ];
     }
@@ -140,7 +132,7 @@ export class NotificationsPage implements OnInit {
     });
 
     await alert.present();
-}
+  }
 //#endregion
 
   logOut() {
